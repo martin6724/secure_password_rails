6b6b4dad7f53c37641d09e181fcc1a3b610d53af
martin6724 <martin.michelleann@gmail.com>
2018-05-01 12:40:45 -0400

    implemented authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+helper_method :current_user
+
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+
+def require_logged_in
+  return true if current_user
+  return redirect_to root_path
+end
 end
